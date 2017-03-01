f25f807d9b3a2d08c14646d4c440eb0622f243b2
superzjn <superzjn@gmail.com>
2017-03-01 11:51:46 -0600

    Initial commit

---
+++ HttpURLConnectionExample.java
@@ -0,0 +1,87 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HttpURLConnectionExample {
+
+    private final String USER_AGENT = "Mozilla/5.0";
+
+    public static void main(String[] args) throws Exception {
+
+        HttpURLConnectionExample http = new HttpURLConnectionExample();
+
+        System.out.println("Testing 1 - Send Http GET request");
+        http.sendGet();
+
+    }
+
+    // HTTP GET request
+    private void sendGet() throws Exception {
+
+        String url = "http://www.google.com/search?q=mkyong";
+
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        // optional default is GET
+        con.setRequestMethod("GET");
+
+        //add request header
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        System.out.println(response.toString());
+
+    }
+
+
+    // HTTP GET request
+    private void getApptoken() throws Exception {
+
+        String TargetUrl = "https://api.sandbox.ebay.com/identity/v1/oauth2/token";
+
+        URL url = new URL(TargetUrl);
+        HttpURLConnection con =  (HttpURLConnection)url.openConnection();
+
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        con.setRequestProperty("Authorization","");
+        int responseCode = con.getResponseCode();
+
+
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        System.out.println(response.toString());
+
+    }
+
+}
